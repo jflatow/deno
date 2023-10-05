85b24d54c7d05afc319397bae3006282ed3851a0
Jared Flatow <jflatow@gmail.com>
2023-10-04 23:30:57 -0700

    Benchmark for server handler args always passing all
    Sample results:
    
    ```
    benchmark           time (avg)        iter/s             (min … max)       p75       p99      p995
    -------------------------------------------------------------------- -----------------------------
    0 handler args       1.33 ms/iter         753.9     (1.12 ms … 2.72 ms)   1.37 ms   2.36 ms    2.5 ms
    1 handler args       1.31 ms/iter         765.5     (1.11 ms … 3.06 ms)   1.33 ms   2.48 ms   2.78 ms
    2 handler args       1.32 ms/iter         755.8    (1.12 ms … 10.16 ms)   1.33 ms   2.49 ms   3.32 ms
    
    benchmark           time (avg)        iter/s             (min … max)       p75       p99      p995
    -------------------------------------------------------------------- -----------------------------
    0 handler args       1.29 ms/iter         773.8     (1.12 ms … 2.34 ms)   1.33 ms   2.16 ms   2.24 ms
    1 handler args       1.32 ms/iter         759.7     (1.12 ms … 4.78 ms)   1.35 ms   2.58 ms   3.22 ms
    2 handler args       1.32 ms/iter         755.8     (1.11 ms … 8.93 ms)   1.35 ms   2.39 ms   2.83 ms
    
    benchmark           time (avg)        iter/s             (min … max)       p75       p99      p995
    -------------------------------------------------------------------- -----------------------------
    0 handler args       1.43 ms/iter         700.9     (1.12 ms … 8.33 ms)   1.41 ms   4.11 ms   6.35 ms
    1 handler args       1.95 ms/iter         511.7     (1.13 ms … 7.48 ms)   2.44 ms   4.93 ms   5.73 ms
    2 handler args       1.37 ms/iter         727.8     (1.11 ms … 4.33 ms)   1.38 ms   3.37 ms   4.21 ms
    
    benchmark           time (avg)        iter/s             (min … max)       p75       p99      p995
    -------------------------------------------------------------------- -----------------------------
    0 handler args       1.45 ms/iter         691.8     (1.14 ms … 3.97 ms)   1.43 ms   3.36 ms   3.56 ms
    1 handler args       1.35 ms/iter         740.1     (1.12 ms … 3.33 ms)   1.38 ms   2.48 ms   2.65 ms
    2 handler args       1.36 ms/iter         736.9    (1.13 ms … 10.55 ms)   1.37 ms   2.62 ms      3 ms
    ```

---
+++ cli/bench/serve.ts
@@ -0,0 +1,41 @@
+// Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+
+import { assert } from '../tests/unit/test_util.ts';
+
+const s0 = Deno.serve({ port: 8080 }, function () {
+  assert(arguments.length == 2);
+  return new Response("hello world");
+});
+
+const s1 = Deno.serve({ port: 8081 }, function (_req) {
+  assert(arguments.length == 2);
+  return new Response("hello world");
+});
+
+const s2 = Deno.serve({ port: 8082 }, function (_req, _info) {
+  assert(arguments.length == 2);
+  return new Response("hello world");
+});
+
+let sum = 0;
+
+Deno.bench(
+  `0 handler args`,
+  async () => {
+    sum += (await (await fetch('http://localhost:8080')).text()).length;
+  },
+);
+
+Deno.bench(
+  `1 handler args`,
+  async () => {
+    sum += (await (await fetch('http://localhost:8081')).text()).length;
+  },
+);
+
+Deno.bench(
+  `2 handler args`,
+  async () => {
+    sum += (await (await fetch('http://localhost:8082')).text()).length;
+  },
+);
